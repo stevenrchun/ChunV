7f8fc10bfe246966baeaae29c62e419532f0e9f4
Steven Chun <steven.r.chun.19@dartmouth.edu>
2019-11-14 17:46:49 -0800

    You are a css superstar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let MillisToSeconds = 1000;
 var heroVideo = document.getElementById("hero-video");
+var videoCover = document.getElementById("cover");
 
 function flipColors() {
   console.log("FLIPPING COLORS");
@@ -19,6 +20,7 @@ function flipColors() {
 setTimeout(function(){
   heroVideo.play();
   heroVideo.classList.toggle("fadein");
+  heroVideo.classList.toggle("fadein-transition");
 
   let whiteoutTargets = document.getElementsByClassName("whiteout-target");
   for (let item of whiteoutTargets){
@@ -29,7 +31,7 @@ setTimeout(function(){
   for (let item of fadeToGreyTargets){
     item.classList.toggle("fadeToGrey");
   }
-}, 7 * MillisToSeconds);
+}, 3 * MillisToSeconds);
 
 // An infinite loop, since it restarts the video.
 heroVideo.addEventListener("ended", function() {
@@ -39,6 +41,17 @@ heroVideo.addEventListener("ended", function() {
     setTimeout(function(){
       heroVideo.play();
       flipColors();
-    }, 30 * MillisToSeconds);
+    }, 3 * MillisToSeconds);
 }, true);
+
+window.addEventListener('scroll', function(e) {
+  //aim for 200px to be enough to fade out
+  heroVideo.classList.toggle("fadein-transition");
+  let y = window.scrollY;
+  let opacity = 1 - (y/200.0);
+  console.log("scroll: " + y);
+  console.log("opacity: " +  opacity);
+  heroVideo.style.opacity = opacity;
+  heroVideo.classList.toggle("fadein-transition");
+});
 
